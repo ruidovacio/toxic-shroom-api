fed42425c4fb1bdc4d3e49c409130298144913d9
ruidovacio <nikkolazhito@hotmail.com>
2023-03-16 10:56:32 -0300

    database cleanup and bug fix

--- index.js
+++ index.js
@@ -7,32 +7,28 @@ const shroombase = require('./database/hongosLista.json')
 
 
 shroomapp.get('/', (req, res) => {
-    res.sendFile(__dirname  + '/public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 })
 
 shroomapp.get('/api/mushrooms', (req, res) => {
     res.send(shroombase);
 })
 
-shroomapp.get('/api/mushrooms/:type',(req,res)=>{
-    const tipo = req.params.type
-    if(tipo != 'poisonous' || tipo != 'deadly'){
+shroomapp.get('/api/mushrooms/:parameter', (req, res) => {
+    const tipo = req.params.parameter;
+    if (tipo === 'poisonous' || tipo === 'deadly') {
         const resultado = shroombase.filter(el => el.type === tipo)
         res.send(resultado);
+    } else if (tipo === 'randomshroom') {
+        const rndIndex = Math.floor(Math.random() * shroombase.length);
+        res.send(shroombase[rndIndex]);
+    } else if (tipo === 'randompic'){
+        const filtroLista = shroombase.filter(el => el.img != '')
+        const rndIndex = Math.floor(Math.random() * filtroLista.length);
+        res.send(filtroLista[rndIndex].img);
     } else {
-        res.status(400).send("Incorrect type");
+        res.status(400).send("Invalid request");
     }
-})
-
-shroomapp.get('/api/mushrooms/randomshroom',(req,res)=>{
-    const rndIndex = Math.floor(Math.random() * shroombase.length);
-    res.send(shroombase[rndIndex]);
-})
-
-shroomapp.get('/api//mushrooms/randompic', (req, res) => {
-    const filtroLista = shroombase.filter(el => el.img != '')
-    const rndIndex = Math.floor(Math.random() * filtroLista.length);
-    res.send(filtroLista[rndIndex].img);
 })
 
 
